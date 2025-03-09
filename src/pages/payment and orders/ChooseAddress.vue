4e7b4f114debda072a68ddd5fbb7914d7f3e96ae
antonios-malak <antoniosmansour7@gmail.com>
2025-03-09 19:38:26 +0200

    updating choose address loading state , empty addresses state and adding new address state .

--- src/pages/payment and orders/ChooseAddress.vue
+++ src/pages/payment and orders/ChooseAddress.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="w-full grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+  <div v-if="isLoading" class="fixed inset-0 bg-gray-50/70 flex justify-center items-center z-50">
     <div
-      class="cursor-pointer"
-      v-for="address in myAddresses"
-      :key="address._id"
-      @click="getPaymentAddress(address._id)"
-    >
-      <AddressComponent :address="address" />
+      class="w-12 h-12 border-4 border-gray-200 border-t-green-500 rounded-full animate-spin"
+    ></div>
+  </div>
+  <div v-if="myAddresses.length == 0">
+    <div class="text-center min-h-[400px]">
+      <p class="text-gray-500 text-lg">You have no addresses yet .</p>
+      <RouterLink :to="{ name: 'addresses' }" class="text-green-500 underline">
+        Add Address and go back to checkout page
+      </RouterLink>
     </div>
+  </div>
+  <div v-else>
+    <div class="w-full grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+      <div
+        class="cursor-pointer"
+        v-for="address in myAddresses"
+        :key="address._id"
+        @click="getPaymentAddress(address._id)"
+      >
+        <AddressComponent :address="address" />
+      </div>
+    </div>
+    <button
+      type="button"
+      class="mx-auto mt-4 md:mt-6 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm md:text-base px-4 md:px-6 py-2 md:py-3 block cursor-pointer w-full sm:w-auto"
+    >
+      <RouterLink :to="{ name: 'addresses' }"> Add another address </RouterLink>
+    </button>
   </div>
 </template>
 
@@ -17,13 +38,14 @@ import { useAuthStore } from '@/assets/Stores/AuthStore'
 import { useCartStore } from '@/assets/Stores/CartStore'
 import AddressComponent from '@/components/AddressComponent.vue'
 import axios from 'axios'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 
 const AddressStore = useAddressStore()
-const myAddresses = AddressStore.addresses
+const myAddresses = computed(() => AddressStore.addresses)
 const auth = useAuthStore()
 const choosenAddress = ref({})
 const cartStore = useCartStore()
+const isLoading = computed(() => AddressStore.isLoadingGetAddresses)
 
 async function getPaymentAddress(id) {
   try {
